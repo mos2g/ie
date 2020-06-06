b4d4527fbc4e36862621aab055a27b0116c11a56
sorush_saghari <tionog.hma@gmail.com>
2020-06-06 23:10:36 +0430

    user migrations

--- ie_root/ie_site/settings.py
+++ ie_root/ie_site/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'user.apps.UserConfig'
 ]
+
+AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
