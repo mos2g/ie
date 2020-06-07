07cac35eb86b34daf9e1537b97bc1fc7973c374c
mos2g <mostafa.gharedaghi@gmail.com>
2020-06-07 09:24:25 +0430

    I really didn't know what to change it's just a test

---
+++ ie_root/user/modelsTest.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('Email', unique=True)
+    first_name = models.CharField('first name', max_length=30, blank=True)
+    last_name = models.CharField('last name', max_length=30, blank=True)
+    date_joined = models.DateTimeField('date joined', auto_now_add=True)
+    is_active = models.BooleanField('active', default=True)
+    is_staff = models.BooleanField('staff', default=False)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
